c3b4f5dd0e5368b62c421fbffbbc001aa9359f38
UdayaSri61001 <s566578@nwmissouri.edu>
2023-10-25 14:59:16 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,18 @@ var router = express.Router();
 // router.get('/', function(req, res, next) {
 //   res.send('respond with a resource');
 // });
+router.get('/computation', function(req, res, next){
+  var x = req.x;
+  if(!x){
+    x = Math.random()*200;
+  }
+  res.send(`Math.floor(${x}) is: ${Math.floor(x)}`);
+});
 
-var sum = 0;
-var more = 0;
 
 router.get('/', function(req,res) {
+  var sum = 0;
+  var more = 0;
   more = more + 1;
   sum += more;
   res.send(`Sum is: ${sum}`);
